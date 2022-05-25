67c9cf2087344dff5ca5df5df2ad59380b759b7b
niqi <syq278872032@gmail.com>
2022-05-25 18:52:42 +0800

    ririsisi-基本结构补全

---
+++ service/service_edu/src/main/java/com/ririsisi/eduservice/EduApplication.java
@@ -0,0 +1,14 @@
+package com.ririsisi.eduservice;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Author ririsisi
+ * @Version 1.0
+ */
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+}
---
+++ service/service_edu/src/main/java/com/ririsisi/eduservice/config/EduConfig.java
@@ -0,0 +1,12 @@
+package com.ririsisi.eduservice.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @Author ririsisi
+ * @Version 1.0
+ */
+@MapperScan("com.ririsisi.eduservice.mapper")
+public class EduConfig {
+
+}
